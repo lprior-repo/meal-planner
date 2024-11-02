a36a49aeee4ff74ebabe75127025e7d6caf13314
lprior-repo <priorlewis43@gmail.com>
2024-11-02 14:21:37 -0500

    read in YAML and parse

--- main.go
+++ main.go
@@ -7,31 +7,49 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
+type Ingredient struct {
+	Name     string `yaml:"name"`
+	Quantity string `yaml:"quantity"`
+}
+
+type Recipe struct {
+	Name         string       `yaml:"name"`
+	Ingredients  []Ingredient `yaml:"ingredients"`
+	Instructions []string     `yaml:"instructions"`
+}
+
 type RecipeStruct struct {
-	Recipes []struct {
-		Name        string `yaml:"name"`
-		Description string `yaml:"description"`
-		Ingredients []struct {
-			Name     string `yaml:"name"`
-			Quantity string `yaml:"quantity"`
-		} `yaml:"ingredients"`
-	} `yaml:"recipes"`
+	Recipes []Recipe `yaml:"recipes"`
 }
 
 func readFile(filename string) ([]byte, error) {
 	recipeFile, err := os.ReadFile(filename)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("couldn't read file %s: %w", filename, err)
 	}
 	return recipeFile, nil
+}
+
+func parseFile(recipeFile []byte) (RecipeStruct, error) {
+	var parseStruct RecipeStruct
+	if err := yaml.Unmarshal(recipeFile, &parseStruct); err != nil {
+		return RecipeStruct{}, fmt.Errorf("couldn't parse the YAML file: %w", err)
+	}
+	return parseStruct, nil
 }
 
 func main() {
 	recipeFile, err := readFile("recipes.yaml")
 	if err != nil {
-		fmt.Println("Couldn't read in recipe file")
+		fmt.Println(err)
+		return
 	}
-	var recipesWrapper RecipeStruct
-	yaml.Unmarshal(recipeFile, &recipesWrapper)
-	fmt.Println(recipesWrapper.Recipes)
+
+	recipes, err := parseFile(recipeFile)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(recipes)
 }
